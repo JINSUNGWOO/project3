e6e2254bb1ecb5efdf9067b22d85485997ae0789
JINSUNGWOO <94368573+JINSUNGWOO@users.noreply.github.com>
2022-04-21 00:15:16 +0900

    2

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from cgitb import html
 from crypt import methods
-from distutils.log import debug
 from flask import Flask, render_template, request
 import os
 
@@ -32,5 +30,4 @@ def my_pick(pick):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-
     app.run(debug=True,host='0.0.0.0',port=port)
